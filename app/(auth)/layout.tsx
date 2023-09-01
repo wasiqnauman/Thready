edd30ad09b1179f9b4a41c88e0307bb03edea37e
wasiqnauman <wasiq_nauman@hotmail.com>
2023-09-02 04:48:17 +0500

    Added project files till leftSideBar

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,28 @@
+import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from 'next/font/google'
+import '../globals.css'
+export const metadata = {
+    title: 'Threads',
+    description: 'A Threads App clone made in Next.js 13 for learning purposes'
+}
+
+
+const inter = Inter({subsets: ['latin']})
+
+
+
+
+export default function RootLayout({children}: {children: React.ReactNode}){
+    return (
+        <ClerkProvider>
+
+            <html lang='en' className={`${inter.className} bg-dark-1`}>
+                <body>
+                    {children}
+                    
+                </body>
+            </html>
+            
+        </ClerkProvider>
+    )
+}
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,44 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "Threads clone made in Next.js 13 for learning purposes",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en" className={`${inter.className} bg-dark-1`}>
+        <body>
+          <Topbar />
+
+          <main className="flex flex-row">
+            <LeftSidebar />
+
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
