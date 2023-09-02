6ffd98f58214de1efb8c3cfd60b293ed4ae009f2
wasiqnauman <wasiq_nauman@hotmail.com>
2023-09-03 01:32:11 +0500

    added dark theme to organization feature and started the onboarding screen

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from "@clerk/themes";
 
 const Topbar = () => {
   return (
@@ -26,6 +27,7 @@ const Topbar = () => {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
