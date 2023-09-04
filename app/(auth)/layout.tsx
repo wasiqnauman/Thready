54211795d6245d67f7c5366222ca9637374075d4
Wasiq Nauman <wasiq_nauman@hotmail.com>
2023-09-04 14:38:21 +0500

    onboarding form implementation
    Added onboarding form

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,28 +1,23 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import { Inter } from 'next/font/google'
-import '../globals.css'
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import "../globals.css";
 export const metadata = {
-    title: 'Threads',
-    description: 'A Threads App clone made in Next.js 13 for learning purposes'
-}
+  title: "Threads",
+  description: "A Threads App clone made in Next.js 13 for learning purposes",
+};
 
+const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({subsets: ['latin']})
-
-
-
-
-export default function RootLayout({children}: {children: React.ReactNode}){
-    return (
-        <ClerkProvider>
-
-            <html lang='en' className={`${inter.className} bg-dark-1`}>
-                <body>
-                    {children}
-                    
-                </body>
-            </html>
-            
-        </ClerkProvider>
-    )
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en" className={`${inter.className} bg-dark-1`}>
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
