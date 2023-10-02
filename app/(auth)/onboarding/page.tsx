c66eb10a9db1a034cefb4bc6b6d00f210fd81b3a
Wasiq Nauman <wasiq_nauman@hotmail.com>
2023-10-02 14:04:33 +0500

    small bug fix

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,10 +1,12 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 
 async function Page() {
   const user = await currentUser();
+  if (!user) return null;
 
-  const userInfo = {};
+  const userInfo = await fetchUser(user.id);
   const userData = {
     id: user?.id,
     objectID: userInfo?._id,
